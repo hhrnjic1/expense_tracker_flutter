fccd4be879d7bfe292135fe3524c302640fb1b0a
Hamza Hrnjic <hamzahrnjic887@gmail.com>
2025-08-16 16:27:50 +0200

    Make icon dynamic and format date with intl

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,13 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 
+final formater = DateFormat.yMd();
 const uuid = Uuid();
 
-enum Category {
-  food,
-  travel,
-  leasure,
-  work
-}
+enum Category { food, travel, leasure, work }
+
+const categortIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight,
+  Category.leasure: Icons.movie,
+  Category.work: Icons.work
+};
 
 class Expense {
   Expense({
@@ -22,4 +27,8 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  get formatedDate {
+    return formater.format(date);
+  }
 }
