748bb8270848288dc2817e2d4ef0a5e17f291b7e
Hamza Hrnjic <hamzahrnjic887@gmail.com>
2025-08-16 14:24:05 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:expenses_tracker_flutter/expenses.dart';
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    )
+  );
+}
+
+
