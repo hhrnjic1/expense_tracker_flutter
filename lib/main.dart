b590f0fea26bd826d240d7262becec8630db8d39
Hamza Hrnjic <hamzahrnjic887@gmail.com>
2025-08-17 18:42:07 +0200

    Add theme with color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:expenses_tracker_flutter/widget/expenses.dart';
+
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromRGBO(255, 96, 69, 181));
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Expenses(),
-    )
-  );
+  runApp(MaterialApp(
+    theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        )),
+    home: Expenses(),
+  ));
 }
-
-
