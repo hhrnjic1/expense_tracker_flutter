f68fcb7c14bc575d431cee85e5a47b0059472f08
Hamza Hrnjic <hamzahrnjic887@gmail.com>
2025-08-17 19:15:27 +0200

    Add dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -3,27 +3,44 @@ import 'package:expenses_tracker_flutter/widget/expenses.dart';
 
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromRGBO(255, 96, 69, 181));
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 5, 99, 125));
 void main() {
   runApp(MaterialApp(
+    darkTheme: ThemeData.dark().copyWith(
+      colorScheme: kDarkColorScheme,
+      appBarTheme: AppBarTheme().copyWith(
+        backgroundColor: kDarkColorScheme.onPrimary,
+      ),
+      cardTheme: CardTheme().copyWith(
+        color: kDarkColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kDarkColorScheme.primaryContainer
+        )
+      ),
+    ),
     theme: ThemeData().copyWith(
-        colorScheme: kColorScheme,
-        appBarTheme: AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer,
-        ),
-        cardTheme: CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-                backgroundColor: kColorScheme.primaryContainer)),
-        textTheme: ThemeData().textTheme.copyWith(
-                titleLarge: TextStyle(
-              fontWeight: FontWeight.normal,
-              color: kColorScheme.onSecondaryContainer,
-              fontSize: 24,
-            ))),
+      colorScheme: kColorScheme,
+      appBarTheme: AppBarTheme().copyWith(
+        backgroundColor: kColorScheme.onPrimaryContainer,
+        foregroundColor: kColorScheme.primaryContainer,
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kColorScheme.primaryContainer
+        )
+      ),
+      cardTheme: CardTheme().copyWith(
+        color: kColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      ),
+    ),
+    themeMode: ThemeMode.system,
     home: Expenses(),
   ));
 }
--- lib/widget/expenses_list/expenses_list.dart
+++ lib/widget/expenses_list/expenses_list.dart
@@ -3,7 +3,8 @@ import 'package:expenses_tracker_flutter/widget/expenses_list/expense_item.dart'
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expensesList, required this.onRemoveExpense});
+  const ExpensesList(
+      {super.key, required this.expensesList, required this.onRemoveExpense});
 
   final List<Expense> expensesList;
   final void Function(Expense expense) onRemoveExpense;
@@ -12,6 +13,11 @@ class ExpensesList extends StatelessWidget {
     return ListView.builder(
       itemCount: expensesList.length,
       itemBuilder: (ctx, index) => Dismissible(
+          background: Container(
+            color: Theme.of(context).colorScheme.error,
+            margin: EdgeInsets.symmetric(
+                horizontal: Theme.of(context).cardTheme.margin!.horizontal),
+          ),
           key: ValueKey(expensesList[index]),
           onDismissed: (direction) {
             onRemoveExpense(expensesList[index]);
