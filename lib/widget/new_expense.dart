764a48a349fdc4e7dc29dd021f31bdb21aef7621
Hamza Hrnjic <hamzahrnjic887@gmail.com>
2025-08-17 17:57:03 +0200

    Display alert dialog on incorect input

--- lib/widget/new_expense.dart
+++ lib/widget/new_expense.dart
@@ -27,6 +27,30 @@ class _NewExpenseState extends State<NewExpense> {
     });
   }
 
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+                title: const Text('Ivalid input'),
+                content: const Text(
+                    'Please make sure a valid title, amount, date and category were entered'),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(ctx);
+                      },
+                      child: const Text('Okay'))
+                ],
+              ));
+      return;
+    }
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -76,6 +100,9 @@ class _NewExpenseState extends State<NewExpense> {
                   )
                 ],
               ),
+              SizedBox(
+                height: 16,
+              ),
               Row(
                 children: [
                   DropdownButton(
@@ -100,10 +127,7 @@ class _NewExpenseState extends State<NewExpense> {
                       },
                       child: const Text('Cancle')),
                   ElevatedButton(
-                      onPressed: () {
-                        print(
-                            '${_titleController.text} && ${_amountController.text}');
-                      },
+                      onPressed: _submitExpenseData,
                       child: const Text('Save Expense')),
                 ],
               )
