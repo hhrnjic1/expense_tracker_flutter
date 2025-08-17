2b4eb4389d410f39ae6434728b12e60805feddff
Hamza Hrnjic <hamzahrnjic887@gmail.com>
2025-08-17 18:51:41 +0200

    Update theme

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,20 @@ void main() {
         appBarTheme: AppBarTheme().copyWith(
           backgroundColor: kColorScheme.onPrimaryContainer,
           foregroundColor: kColorScheme.primaryContainer,
-        )),
+        ),
+        cardTheme: CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: kColorScheme.primaryContainer)),
+        textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+              fontWeight: FontWeight.normal,
+              color: kColorScheme.onSecondaryContainer,
+              fontSize: 24,
+            ))),
     home: Expenses(),
   ));
 }
